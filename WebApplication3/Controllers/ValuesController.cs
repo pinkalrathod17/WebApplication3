42b2fba5ba8f7ede8f3cf5c2c166022407f62a39
pinkal.rathod <pinkal.rathod>
2020-04-13 13:39:07 +0530

    commit

--- WebApplication3/Controllers/ValuesController.cs
+++ WebApplication3/Controllers/ValuesController.cs
@@ -14,6 +14,7 @@ namespace WebApplication3.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            //return new string[] { "value1", "value2" };
             return new string[] { "value1", "value2" };
         }
 
